af77da79cb00ebcdc7428b5e4a736f4d7a8c4226
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-03-10 21:49:10 +0100

    Added balanced randomness

--- source/BREngine.cpp
+++ source/BREngine.cpp
@@ -1,10 +1,12 @@
 #include "BREngine.h"
 
-BREngine::BREngine(int min, int max)
+BREngine::BREngine(int min, int max, float deviations)
 	: m_MinResult{ min }
 	, m_MaxResult{ max }
 	, m_Mean{ (min + max) / 2.f }
+	, m_StandardDeviation{ CalculateStandardDeviation(min, max) }
 	, m_Distribution{ min, max }
+	, m_Deviations{ deviations }
 {
 
 }
@@ -12,6 +14,45 @@ BREngine::BREngine(int min, int max)
 int BREngine::Generate()
 {
 	int result{ m_Distribution(m_RD) };
+
+	bool valid{ false };
+	do
+	{
+		result = m_Distribution(m_RD);
+
+		float cumulativeMean{ (m_AmountGenerated + 1) * m_Mean };
+		float cumulativeDeviation{ sqrt(static_cast<float>(m_AmountGenerated + 1)) * m_StandardDeviation };
+
+		if (m_Sum + result > cumulativeMean - cumulativeDeviation * m_Deviations &&
+			m_Sum + result < cumulativeMean + cumulativeDeviation * m_Deviations)
+		{
+			valid = true;
+		}
+
+	} while (!valid);
+
+	m_Sum += result;
+	++m_AmountGenerated;
+
+	return result;
+}
+
+void BREngine::SetDeviations(float deviations)
+{
+	m_Deviations = deviations;
+}
+
+float BREngine::CalculateStandardDeviation(int min, int max)
+{
+	int count{ min + max - 1 };
+	float mean{ (min + max) / 2.f };
+
+	float result{};
+	for (int index{ 1 }; index <= count; ++index)
+	{
+		result += (index - mean) * (index - mean);
+	}
+	result = sqrt(result / count);
 
 	return result;
 }
--- source/BREngine.h
+++ source/BREngine.h
@@ -6,17 +6,26 @@
 class BREngine final
 {
 public:
-	BREngine(int min, int max);
+	BREngine(int min, int max, float deviations = 2.f);
 
 	int Generate();
+
+	void SetDeviations(float deviations);
 
 	const int m_MinResult;
 	const int m_MaxResult;
 	const float m_Mean;
+	const float m_StandardDeviation;
 
 private:
+	static float CalculateStandardDeviation(int min, int max);
+
 	std::random_device m_RD; //unitialized by design
 	std::uniform_int_distribution<int> m_Distribution;
+
+	float m_Deviations;
+	int m_Sum{};
+	int m_AmountGenerated{};
 };
 
 #endif
