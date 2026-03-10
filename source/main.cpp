cdf79250ae4fe79541f2b912017372ee6200d0f1
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-03-10 21:49:25 +0100

    Added testing functions to main

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,84 @@
 #include <iostream>
+
 #include "BREngine.h"
+
+struct Data
+{
+	std::vector<int> results;
+};
+
+void TestEngine(int min, int max, int numberCount, int sampleCount, float deviation = 2.f)
+{
+	std::vector<Data> samples{ };
+
+	for (int sample{ 0 }; sample < sampleCount; ++sample)
+	{
+		Data sampleData{};
+		BREngine brEngine{ min, max, deviation };
+
+		for (int iteration{ 0 }; iteration < numberCount; ++iteration)
+		{
+			int result{ brEngine.Generate() };
+			sampleData.results.push_back(std::move(result));
+		}
+
+		samples.push_back(std::move(sampleData));
+	}
+
+	float minMean{ FLT_MAX };
+	float maxMean{ FLT_MIN };
+	float averageMean{};
+
+	float minSum{ FLT_MAX };
+	float maxSum{ FLT_MIN };
+	float averageSum{};
+
+	for (const Data& data : samples)
+	{
+		float mean{};
+		float Sum{};
+		for (int result : data.results)
+		{
+			mean += result;
+			Sum += result;
+		}
+		mean /= numberCount;
+
+		if (mean < minMean)
+		{
+			minMean = mean;
+		}
+		else if (mean > maxMean)
+		{
+			maxMean = mean;
+		}
+		averageMean += mean;
+
+		if (Sum < minSum)
+		{
+			minSum = Sum;
+		}
+		else if (Sum > maxSum)
+		{
+			maxSum = Sum;
+		}
+		averageSum += Sum;
+	}
+	averageMean /= sampleCount;
+	averageSum /= sampleCount;
+
+	std::cout << "- - " << sampleCount << " samples taken - - " << numberCount << " numbers per sample - - deviation is " << deviation << " - -\n";
+	std::cout << "Type\tMin\tMax\tAverage\n\n";
+	std::cout << "Mean\t" << minMean << "\t" << maxMean << "\t" << averageMean << "\n";
+	std::cout << "Sum\t" << minSum << "\t" << maxSum << "\t" << averageSum << "\n";
+	std::cout << "\n";
+}
 
 int main()
 {
-	BREngine brEngine{ 1, 20 };
-	std::cout << "Random number: " << brEngine.Generate() << "\n";
-	std::cout << "Random number: " << brEngine.Generate() << "\n";
-	std::cout << "Random number: " << brEngine.Generate() << "\n";
-	std::cout << "Random number: " << brEngine.Generate() << "\n";
+	TestEngine(1, 20, 20, 10000, 0.2f);
+	TestEngine(1, 20, 20, 10000, 1.f);
+	TestEngine(1, 20, 20, 10000, 2.f);
+	TestEngine(1, 20, 20, 10000, 3.f);
+	TestEngine(1, 20, 20, 10000, 1000.f);
 }
