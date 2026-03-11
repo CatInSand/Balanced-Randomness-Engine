0daaf555516adbb6b8516a36fcff6120421d28c6
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-03-11 11:15:13 +0100

    Added additional comments

--- source/BREngine.h
+++ source/BREngine.h
@@ -2,12 +2,17 @@
 #define BRENGINE_H
 
 #include <random>
+#include <vector>
 
+/**
+A random number generator that balances results by ensuring the sum of all results
+stays within a specified number of standard deviations away from the average.
+*/
 class BREngine final
 {
 public:
 	/**
-	* Constructs a balanced randomness engine
+	* Constructs a balanced randomness engine.
 	*
 	* @param min: Minimum value the engine can generate.
 	* @param max: Maximum value the engine can generate.
@@ -16,23 +21,36 @@ public:
 	BREngine(int min, int max, float deviations = 2.f);
 
 	/**
-	* Generates a random number between the minimum and maximum
+	* Generates a random number between the minimum and maximum.
 	*
 	* @return The generated number.
 	*/
 	int Generate();
 
 	/**
-	* Resets the internal counters that track previous results
-	*
+	Resets the internal counters that track previous results.
 	*/
 	void Reset();
 
+	/**
+	The minimum result the engine can generate.
+	*/
 	const int m_MinResult;
+	/**
+	The maximum result the engine can generate.
+	*/
 	const int m_MaxResult;
+	/**
+	The average result the engine will generate.
+	*/
 	const float m_Mean;
+	/**
+	The value of a single standard deviation.
+	*/
 	const float m_StandardDeviation;
-
+	/**
+	The number of standard deviations the sum of results will stay within.
+	*/
 	float m_Deviations;
 
 	/**
@@ -41,7 +59,6 @@ public:
 	* @return The sum.
 	*/
 	int Sum() const;
-
 	/**
 	* Getter for the internal count of generated numbers.
 	*
